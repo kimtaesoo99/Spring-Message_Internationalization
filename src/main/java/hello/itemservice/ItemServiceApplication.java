57926b85fdedd82d54583ac8b27dabd5fb4ca4ae
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-02 12:03:55 +0900

    feat: 메서지 소스를 빈으로 등록

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -10,4 +13,12 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+//	스프링부트가 자동으로 빈으로 등록해줌
+//	@Bean
+//	public MessageSource messageSource(){
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages","errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 }
